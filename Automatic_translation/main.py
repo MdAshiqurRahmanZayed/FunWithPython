7ed725dd56f6320cf9260d2e4354425015061dcb
mdzayed2019@gmail.com <mdzayed2019@gmail.com>
2022-03-31 22:29:19 +0600

    Automatic_translations

---
+++ Automatic_translation/main.py
@@ -0,0 +1,15 @@
+#using goslate pip install goslate
+from textblob import TextBlob
+import goslate
+
+text = "python is a powerful language"
+new_translate = goslate.Goslate()
+translations =new_translate.translate(text, 'fr')
+print(translations)
+
+#using from textblob import TextBlob | pip install textblob
+text = TextBlob("Python is a strong language")
+text.translate(to='ar')
+text = TextBlob("Python is a strong language")
+translations = text.translate(to = 'ar')
+print(translations)
